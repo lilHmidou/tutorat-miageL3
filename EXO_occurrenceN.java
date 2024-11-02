21767399670fc70a02798125d97da739ac917e5d
Nazim6702 <nazimomr23@gmail.com>
2024-11-02 19:06:28 +0100

    Nazim - EXO_occurrenceN- 02/11/2024

--- EXO_occurrenceN.java
+++ EXO_occurrenceN.java
@@ -2,26 +2,20 @@ public class EXO_occurrenceN {
 
     public static int occurrenceN(int[] tab, int valeur, int n) {
         int occurrence = 0;
-        int indice = -1;
         for (int i = 0; i < tab.length; i++) {
             if (tab[i] == valeur) {
                 occurrence++;
                 if (occurrence == n) {
-                    indice = i;
+                    return i;
                 }
             }
         }
-
-        if (n > occurrence) {
-            return -1;
-        }
-
-        return indice;
+        return -1;
     }
 
     public static void main(String[] args) {
         int[] tab = { 0, 2, 5, 6, 2, 2 };
-        System.out.println(EXO_occurrenceN.occurrenceN(tab, 2,2));
+        System.out.println(EXO_occurrenceN.occurrenceN(tab, 6,1));
     }
     
 }
